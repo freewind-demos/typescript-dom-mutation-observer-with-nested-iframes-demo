8f26e75b7339145af17da666b48ea6c16d3f3055
Freewind <nowindlee@gmail.com>
2020-12-08 15:39:53 +0800

    typescript-dom-mutation-observer-with-nested-iframes-demo

---
+++ README.md
@@ -0,0 +1,9 @@
+TypeScript DOM MutationObserver with Nested IFrames Demo
+========================================================
+
+对于Nested iframes，可以在每个iframe的document中设置 MutationObserver
+
+```
+npm i
+npm run demo
+```
--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,42 @@
-console.log('### Hello');
+function enhanceButton() {
+  const nested1 = document.querySelector('iframe')!;
+  const nested2 = nested1.contentDocument!.querySelector('iframe')!;
+  const button = nested2.contentDocument!.querySelector('#button')!
+
+  button.addEventListener('click', () => {
+    const div = nested2.contentDocument?.createElement('div')!;
+    div.textContent = 'new created';
+    nested2.contentDocument!.body.appendChild(div);
+  })
+}
+
+function setupMutationObserver() {
+  function setupPage(win: Window) {
+    console.log('### setupPage', win);
+    const targetNode = win.document.querySelector('body')!;
+
+    const observerOptions = {
+      childList: true, // direct children
+      attributes: true,
+      subtree: true // descendants. Only available when childList is true
+    };
+
+    const observer = new MutationObserver(function () {
+      console.log('DOM is changed on ', win);
+    });
+
+    observer.observe(targetNode, observerOptions);
+
+    const iframes = Array.from(win.document.querySelectorAll('iframe'));
+    iframes.forEach((iframe) => {
+      setupPage(iframe.contentWindow!);
+    })
+  }
+
+  setupPage(window);
+}
+
+setTimeout(() => {
+  enhanceButton();
+  setupMutationObserver();
+}, 1000);
